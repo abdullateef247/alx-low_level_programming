1228c6264e747e7b6106420dcd780bc82f6dabd1
abdullateef247 <kayode.olawale247@gmail.com>
2023-10-06 16:02:00 +0100

    fix code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
  *
  * Return: Always return 0 (success)
  */
-int main (void)
+int main(void)
 {
-    char str[] = "\"Programming is like building a multilingual puzzle";
+char str[] = "\"Programming is like building a multilingual puzzle";
 
-    puts(str);
-    return 0;
+puts(str);
+return (0);
 }
