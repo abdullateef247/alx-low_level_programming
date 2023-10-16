9c4d75c48bf5db0ebee11e8534bb911357e0f8c5
abdullateef247 <kayode.olawale247@gmail.com>
2023-10-16 18:14:57 +0100

    write the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Return the length of a string
+ * @s: String value
+ *
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
