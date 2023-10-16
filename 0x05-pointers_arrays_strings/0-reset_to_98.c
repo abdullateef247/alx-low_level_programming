92afcdc45f63d761f71106184b65442ac8b638a2
abdullateef247 <kayode.olawale247@gmail.com>
2023-10-16 17:02:20 +0100

    reset value of parameter to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value of parameter to 98
+ * @n: the value to be changed
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
