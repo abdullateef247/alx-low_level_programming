1e0d88f1b0fc654b84c2582c9323e587f87a8001
abdullateef247 <kayode.olawale247@gmail.com>
2023-10-17 05:43:18 +0100

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - print reverse
+ * @s: string
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int len = strlen(s);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
