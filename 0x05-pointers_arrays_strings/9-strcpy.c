7de617cb67fb5ed8515e446a2cb93632791e79f3
abdullateef247 <kayode.olawale247@gmail.com>
2023-10-17 05:10:37 +0100

    copies string to destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string from source to destination
+ * @dest: destination
+ * @src: source
+ *
+ * Return: Character
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *original_dest = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (original_dest);
+}
