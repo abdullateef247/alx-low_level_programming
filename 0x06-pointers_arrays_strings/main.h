e1c58f647e49147e3ef8f4649ab4c870c039eead
abdullateef247 <kayode.olawale247@gmail.com>
2023-10-20 04:36:09 +0100

    update prototype

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,5 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *_strncat(char *dest, char *src, int n);
-
+char *_strncpy(char *dest, char *src, int n);
 #endif
