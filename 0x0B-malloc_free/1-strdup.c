dd3ce141120d04e068cf148f342a173a1358355f
abdullateef247 <kayode.olawale247@gmail.com>
2023-10-31 16:21:03 +0100

    return a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - make pointer to a newly allocated space in memory
+ * @str: string
+ *
+ * Return: pointer on succes or null
+ */
+
+char *_strdup(char *str)
+{
+	int i, n;
+	char *ptr_s;
+
+	if (str == NULL)
+	{
+		return (NULL); /* no argument supplied */
+	}
+
+	n = strlen(str); /* counter in loop */
+	ptr_s = malloc(n * sizeof(char));
+
+	if (ptr_s == NULL)
+	{
+		return (NULL); /* Error allocating memory*/
+	}
+	for (i = 0; i < n; i++)
+	{
+		ptr_s[i] = str[i];
+	}
+	return (ptr_s);
+}
