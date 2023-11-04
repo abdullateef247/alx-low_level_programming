d5fd772206bccde83eff3271d61d77bf20076df0
abdullateef247 <kayode.olawale247@gmail.com>
2023-11-04 04:08:36 +0100

    concatenate two strings with first n bytes of second string

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates two strings with n bytes of the second string
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ *
+ * Return: pointer to new memory location or NULL
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *result, *temp;
+	unsigned int len1 = 0, len2 = 0, total_len = 0;
+
+	/* Handle NULL strings */
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	/* Calculate the length of s1 */
+	while (s1[len1])
+	{
+		len1++;
+	}
+
+	/* Calculate the length of s1 */
+	while (s2[len2])
+	{
+		len2++;
+	}
+
+	/* Determine the actual number of bytes to concatenate from s2 */
+	if (n >= len2)
+	{
+		n = len2;
+	}
+
+	/* Calculate the total length of the result string */
+	total_len = len1 + n;
+
+	/* Allocate memory for the result string */
+	result = malloc(total_len + 1);
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	
+	/*copy first string s1*/
+	temp = result;
+	while (s1)
+	{
+		*temp = *s1;
+		temp++;
+		s1++;
+	}
+
+	/* copy first n bytes */
+	while (n > 0)
+	{
+		*temp = *s2;
+		temp++;
+		s2++;
+		n--;
+	}
+	*temp = '\0';
+	return (result);
+}	
